6550da8ce96297957e127e3e9b77f50fc5d4908c
Lee-GS <leegs4831@naver.com>
2023-07-25 23:54:21 +0900

    23/07/25 solve

---
+++ src/main/kotlin/달리기 경주_프로그래머스.py.kt
@@ -0,0 +1,19 @@
+fun solution(players: Array<String>, callings: Array<String>): Array<String> {
+
+    val rankMap = mutableMapOf<String, Int>()
+    players.forEachIndexed { index, s -> rankMap[s] = index }
+
+    callings.forEachIndexed { index, s ->
+        val callRank = rankMap[s] ?: 0
+        val frontPlayer = players[callRank - 1]
+
+        players[callRank - 1] = s
+        players[callRank] = frontPlayer
+
+        rankMap[s] = callRank - 1
+        rankMap[frontPlayer] = callRank
+    }
+
+
+    return players
+}
